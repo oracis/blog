efe1833874d579640bb00c0b196fae074146c662
Robin Zhou <origin@gmail.com>
2022-01-05 18:19:53 +0800

    use iconfont & optimize admin

--- src/admin/container/homeManagement/index.jsx
+++ src/admin/container/homeManagement/index.jsx
@@ -1,49 +1,36 @@
 import React, { useState } from "react";
 import { Layout, Menu } from 'antd';
-import {
-    MenuUnfoldOutlined,
-    MenuFoldOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-} from '@ant-design/icons';
 import style from "./style.module.scss";
 
 const { Header, Sider, Content } = Layout;
 
-const HomeManagement = () => {
+const useCollapsed = () => {
     const [collapsed, setCollapsed] = useState(false);
     const toggle = () => setCollapsed(!collapsed);
+    return { collapsed, toggle };
+}
+
+const HomeManagement = () => {
+    const { collapsed, toggle } = useCollapsed();
     const handleHomePageRedirect = () => window.location.href="/";
 
     return (
         <Layout>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                        首页内容管理
+            <Sider className={style.sidebar} trigger={null} collapsible collapsed={collapsed}>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['admin-home']}>
+                    <Menu.Item key="admin-home">
+                        <span className="iconfont">&#xe69b;</span>首页内容管理
                     </Menu.Item>
-                    <Menu.Item key="2" icon={<VideoCameraOutlined />} onClick={handleHomePageRedirect}>
-                        返回用户首页
+                    <Menu.Item key="admin-back" onClick={handleHomePageRedirect}>
+                        <span className="iconfont">&#xe635;</span>返回用户首页
                     </Menu.Item>
                 </Menu>
             </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }}>
-                    {
-                        collapsed
-                        ? <MenuUnfoldOutlined className={`${style.trigger} ${style.anticon}`} onClick={toggle} />
-                        : <MenuFoldOutlined className={`${style.trigger} ${style.anticon}`} onClick={toggle} />
-                    }
+            <Layout>
+                <Header className={style.header}>
+                    <span className="iconfont" onClick={toggle} dangerouslySetInnerHTML={{__html: collapsed ? "&#xe62c;" : "&#xe629;"}} />
                 </Header>
-                <Content
-                    className="site-layout-background"
-                    style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        height: 'calc(100vh - 112px)'
-                    }}
-                >
+                <Content className={style.content}>
                     Content
                 </Content>
             </Layout>
--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import HomeManagement from "./container/homeManagement/";
 import "normalize.css";
 import "antd/dist/antd.min.css";
+import "./style.scss";
 
 ReactDOM.render(
   <React.StrictMode>
